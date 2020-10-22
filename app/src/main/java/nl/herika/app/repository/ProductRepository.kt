4c50da4b665b18f02f9fc224d955a2d57faaa7b7
Alexander van den Herik <alexander.van.den.herik2@hva.nl>
2020-10-23 00:44:57 +0200

    Done with 2.*

---
+++ app/src/main/java/nl/herika/app/repository/ProductRepository.kt
@@ -0,0 +1,34 @@
+package nl.herika.app.repository
+
+import android.content.Context
+import nl.herika.app.dao.ProductDao
+import nl.herika.app.database.ShoppingListRoomDatabase
+import nl.herika.app.model.Product
+
+
+class ProductRepository(context: Context) {
+
+    private val productDao: ProductDao
+
+    init {
+        val database = ShoppingListRoomDatabase.getDatabase(context)
+        productDao = database!!.productDao()
+    }
+
+    suspend fun getAllProducts(): List<Product> {
+        return productDao.getAllProducts()
+    }
+
+    suspend fun insertProduct(product: Product) {
+        productDao.insertProduct(product)
+    }
+
+    suspend fun deleteProduct(product: Product) {
+        productDao.deleteProduct(product)
+    }
+
+    suspend fun deleteAllProducts() {
+        productDao.deleteAllProducts()
+    }
+
+}
